223c8aa0f6491f179398f89e0107273bf2f9032e
Matt Harden <matt.harden@gmail.com>
2015-01-29 07:00:09 -0600

    Uses a channel as a chunked ring buffer

--- shootout/mattharden/mattharden.go
+++ shootout/mattharden/mattharden.go
@@ -6,18 +6,42 @@ import (
 )
 
 func Copy(w io.Writer, r io.Reader, buflen int) (int64, error) {
-	pr, pw := io.Pipe()
-	go func() {
-		_, err := io.Copy(pw, bufio.NewReaderSize(r, buflen))
-		pw.CloseWithError(err)
-	}()
-	bw := bufio.NewWriterSize(w, buflen)
-	n, err := io.Copy(bw, pr)
-	if err == nil {
-		err = bw.Flush()
-	} else {
-		// Ignoring error from Flush in favor of the previous error
-		bw.Flush()
+	return bufcopy(w, r, buflen/16, 16)
+}
+
+func bufcopy(w io.Writer, r io.Reader, buflen int, count int) (int64, error) {
+	c_in := make(chan []byte, count)
+	c_out := make(chan []byte)
+	c_err := make(chan error)
+	for i := 0; i < count; i++ {
+		c_in <- make([]byte, buflen)
 	}
-	return n - int64(bw.Buffered()), err
+	go func() {
+		var err error
+		for err == nil {
+			buf := <-c_in
+			var n int
+			n, err = r.Read(buf)
+			buf = buf[:n]
+			c_out <- buf
+		}
+		if err == io.EOF {
+			err = nil
+		}
+		c_err <- err
+	}()
+	var length int64
+	for {
+		select {
+		case err := <-c_err:
+			return length, err
+		case buf := <-c_out:
+			n, err := w.Write(buf)
+			length += int64(n)
+			if err != nil {
+				return length, err
+			}
+			c_in <- buf[:cap(buf)]
+		}
+	}
 }
