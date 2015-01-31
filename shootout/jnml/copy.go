db3d38682c67986984b1503cb8cf2d19f2d61f87
Jan Mercl <0xjnml@gmail.com>
2015-01-31 17:29:20 +0100

    Solve the problem.

--- shootout/jnml/copy.go
+++ shootout/jnml/copy.go
@@ -3,61 +3,112 @@ package jnml
 import "io"
 
 func Copy(dst io.Writer, src io.Reader, buffer int) (int64, error) {
-	type t struct {
-		err error
-		b   []byte
-	}
-	page := 1 << 20
-	if buffer < page {
-		page = buffer
-	}
-	w := make(chan []byte, buffer/page+2)
-	r := make(chan t, buffer/page+2)
+	b := make([]byte, buffer)
+	w := make(chan int, 16)
+	r := make(chan int, 16)
+	var werr error
 	go func() {
-		for chunk := range w {
-			c0 := chunk
-			for len(chunk) != 0 {
-				n, err := dst.Write(chunk)
-				if err != nil {
-					r <- t{err: err}
+		var d, i, n, t int
+		for q := range w {
+			if q < 0 {
+				return
+			}
+			for t += q; t != 0; t -= n {
+				if n = i + t; n > buffer {
+					n = buffer
+				}
+				var err error
+				if n, err = dst.Write(b[i:n]); err != nil {
+					werr = err
+					r <- -1
 					return
 				}
-				chunk = chunk[n:]
+				if i += n; i == buffer {
+					i = 0
+				}
+				d += n
+				select {
+				case r <- d:
+					d = 0
+				default:
+				}
+				select {
+				case q = <-w:
+					if q < 0 {
+						return
+					}
+					t += q
+				default:
+				}
 			}
-			r <- t{b: c0[:page]}
 		}
-		r <- t{}
+		r <- 0
 	}()
-	pages := make([][]byte, buffer/page)
-	buf := make([]byte, buffer)
-	for i := range pages {
-		pages[i] = buf[i*page : i*page+page]
-	}
-	for nn := int64(0); ; {
-		b := pages[len(pages)-1]
-		pages = pages[:len(pages)-1]
-		n, err := src.Read(b)
-		if n != 0 {
-			nn += int64(n)
-			w <- b[:n]
-			buffer -= page
+	var i, j, p, q int
+	var nn int64
+	var s []byte
+	for {
+		switch {
+		case p == 0:
+			s = b[i:buffer]
+		case i == j:
+			n := <-r
+			if n == -1 {
+				return nn, werr
+			}
+			p -= n
+			if j += n; j >= buffer {
+				j -= buffer
+				continue
+			}
+		case i > j:
+			s = b[i:buffer]
+		default:
+			s = b[i:j]
 		}
-		var x t
+		n, err := src.Read(s)
+		if n != 0 {
+			q += n
+			select {
+			case w <- q:
+				q = 0
+			default:
+			}
+			p += n
+			if i += n; i == buffer {
+				i = 0
+			}
+			nn += int64(n)
+			select {
+			case n = <-r:
+				if n < 0 {
+					return nn, werr
+				}
+				p -= n
+				if j += n; j >= buffer {
+					j -= buffer
+					continue
+				}
+			default:
+			}
+		}
 		if err != nil {
-			close(w)
 			if err != io.EOF {
+				w <- -1
 				return nn, err
 			}
-			for x = <-r; x.b != nil; x = <-r {
+			if q != 0 {
+				w <- q
 			}
-			return nn, x.err
-		}
-		for buffer < page {
-			if x = <-r; x.b == nil {
-				return nn, x.err
+			close(w)
+			for {
+				switch n = <-r; n {
+				case -1:
+					return nn, werr
+				case 0:
+					return nn, nil
+				}
 			}
-			buffer += page
-			pages = append(pages, b)
 		}
 	}
 }
